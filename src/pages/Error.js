58bee5e685fad2ba8e2c9715cd802e58d848ebce
AnthonyGKruger <60028700+AnthonyGKruger@users.noreply.github.com>
2023-04-13 05:37:38 +0200

    initial push to redux branch

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -26,9 +26,6 @@ const Error = () => {
 			<Header />
 			<section className="vh-90 avenir">
 				<header className="tc ph5 lh-copy">
-					{/* <h1 className="f1 f-headline-l code mb3 fw9 dib tracked-tight light-purple">
-						404
-					</h1> */}
 					<Lottie width={lottieWidth} height={lottieHeight} options={defaultOptions}/>
 					<h2 className="tc f1-l fw1">
 						Sorry, we can't find the page you are looking for.
@@ -37,56 +34,6 @@ const Error = () => {
 				<p className="fw1 i tc mt4 mt5-l f4 f3-l">
 					Try using the navigation above to find the page you are looking for.
 				</p>
-				{/* <ul className="list tc pl0 w-100 mt5">
-					<li className="dib">
-						<Link
-							className="f5 f4-ns link black db pv2 ph3 hover-light-purple"
-							href="/"
-						>
-							Home
-						</Link>
-					</li>
-					<li className="dib">
-						<Link
-							className="f5 f4-ns link black db pv2 ph3 hover-light-purple"
-							href="/about"
-						>
-							About
-						</Link>
-					</li>
-					<li className="dib">
-						<Link
-							className="f5 f4-ns link black db pv2 ph3 hover-light-purple"
-							href="/careers"
-						>
-							Careers
-						</Link>
-					</li>
-					<li className="dib">
-						<Link
-							className="f5 f4-ns link black db pv2 ph3 hover-light-purple"
-							href="/contact"
-						>
-							Contact
-						</Link>
-					</li>
-					<li className="dib">
-						<Link
-							className="f5 f4-ns link black db pv2 ph3 hover-light-purple"
-							href="/signup"
-						>
-							Sign Up
-						</Link>
-					</li>
-					<li className="dib">
-						<Link
-							className="f5 f4-ns link black db pv2 ph3 hover-light-purple"
-							href="/help"
-						>
-							Help
-						</Link>
-					</li>
-				</ul> */}
 			</section>
 			<Footer />
 		</>
